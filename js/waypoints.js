66eb52dc4b9cf1ee089c1f975bfa9c8dd1313d8c
Bargee <b.geers@dreamlake.nl>
2016-08-10 14:04:09 +0200

    working wayponts

--- js/waypoints.js
+++ js/waypoints.js
@@ -1,9 +1,27 @@
-var waypoint = new Waypoint({
-  element: document.getElementById('popup1'),
-  handler: function(direction) {
-    console.log('I am 20px from the top of the window');
-  },
-  offset: function() {
-    return this.element.clientHeight
+//var waypoint = new Waypoint({
+//  element: document.getElementById('popup1'),
+//  handler: function(direction) {
+//    console.log('I am 20px from the top of the window');
+//  },
+//  offset: function() {
+//    return this.element.clientHeight
+//  }
+//});
+
+//var $popup1 = $('.popup1');
+//
+//$popup1.waypoint(function () {
+//    console.log('test');
+//});
+//
+
+
+var $dipper = $('.dipper');
+
+$dipper.waypoint(function (direction) {
+  if (direction == 'down') {
+    $dipper.addClass('js-dipper-animate');
+  } else {
+    $dipper.removeClass('js-dipper-animate');
   }
-});
+}, { offset: '50%' });
